014255698cf26042b7db5ab303124558c7af67af
“OmalPerera” <omalperera8@gmail.com>
2020-02-22 16:41:10 +0530

    #6 : shallow implementation in role mgmt

--- src/controllers/users/login.controller.ts
+++ src/controllers/users/login.controller.ts
@@ -15,13 +15,44 @@ export function authenticatingUsers(reqPayload: any) {
                     "value": "sk_test_4eC39Hq-_test_4C9HqLyjWDarjtT-1zdp7dcLyjWDa-rjtT1zdp7d",
                     "expiredAfter": "2020-02-03T23:09:04.431Z"
                 },
-                "user_id": 167,
-                "user_name": "jone",
+                "user_id": 1,
+                "user_name": "Jone Ad",
                 "role": "admin"
             }
         }
         return responsePayload;
-    } else {
+    }
+    else if (reqPayload.userEmail === 'driver@harvestcraft.com' && reqPayload.userPassword === 'hc@123') {
+        responsePayload = {
+            "status": 200,
+            "data": {
+                "token": {
+                    "value": "sk_test_4eC39Hq-_test_4C9HqLyarjtT-1zdp7dcLyjWDa-rjtT1zdp7d",
+                    "expiredAfter": "2020-03-03T23:09:04.431Z"
+                },
+                "user_id": 2,
+                "user_name": "Thomas Dri",
+                "role": "driver"
+            }
+        }
+        return responsePayload;
+    }
+    else if (reqPayload.userEmail === 'store@harvestcraft.com' && reqPayload.userPassword === 'hc@123') {
+        responsePayload = {
+            "status": 200,
+            "data": {
+                "token": {
+                    "value": "sk_test_4C39Hq-_test_4C9HqLyarjtT-1zdp7dcyjWDa-rjtT1zdp7d",
+                    "expiredAfter": "2020-03-03T23:09:04.431Z"
+                },
+                "user_id": 3,
+                "user_name": "Kathrine Sm",
+                "role": "storemanager"
+            }
+        }
+        return responsePayload;
+    }
+    else {
         responsePayload = {
             "status": 412,
             "data": {
